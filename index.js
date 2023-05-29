88e5ecd3ab93e63229d65a017dd7cbad0dd149b1
TyronNhatAnh <tyron.nguyen@gogox.com>
2023-05-29 15:41:21 +0700

    update dynamic resize image

--- index.js
+++ index.js
@@ -1,86 +1,115 @@
-const AWS = require('aws-sdk');
-const sharp = require('sharp');
-                
+const AWS = require("aws-sdk");
+const sharp = require("sharp");
+
 // get reference to S3 client
 const s3 = new AWS.S3();
 
-// const RESIZED_BUCKET = process.env.RESIZED_BUCKET;
-// const ALLOWED_DIMENSIONS = new Set();
+const RESIZED_BUCKET = process.env.RESIZED_BUCKET;
+const ALLOWED_DIMENSIONS = new Set();
 
-// // Allowed dimensions format: "wxh,16x16,28x28"
-// if (process.env.ALLOWED_DIMENSIONS) {
-//     const dimensions = process.env.ALLOWED_DIMENSIONS.split(",");
-//     dimensions.forEach((dimension) => ALLOWED_DIMENSIONS.add(dimension));
-// }
+// Allowed dimensions format: "wxh,16x16,28x28"
+if (process.env.ALLOWED_DIMENSIONS) {
+  const dimensions = process.env.ALLOWED_DIMENSIONS.split(",");
+  dimensions.forEach(dimension => ALLOWED_DIMENSIONS.add(dimension));
+}
 
-function getExtension (fileName) {
+function getExtension(fileName) {
   const split = fileName.split(".");
   return split[split.length - 1];
-};
+}
 
 async function handleNoSize(fileName, coldBucket, s3) {
   const fileExtension = getExtension(fileName);
 
-  const uploaded = await s3.getObject({ Bucket: coldBucket, Key: fileName }).promise();
+  const uploaded = await s3
+    .getObject({Bucket: coldBucket, Key: fileName})
+    .promise();
 
   return {
-      statusCode: 200,
-      headers: { "Content-Type": "application/" + fileExtension, "Content-Disposition": `attachment; filename=${fileName}` },
-      body: uploaded.Body?.toString("base64") || "",
-      isBase64Encoded: true,
+    statusCode: 200,
+    headers: {
+      "Content-Type": "application/" + fileExtension,
+      "Content-Disposition": `attachment; filename=${fileName}`,
+    },
+    body: uploaded.Body?.toString("base64") || "",
+    isBase64Encoded: true,
   };
-};
+}
 
 async function handleResized(key, resizedBucket, s3) {
   const fileExtension = getExtension(key);
 
-  const uploaded = await s3.getObject({ Bucket: resizedBucket, Key: key }).promise();
+  const uploaded = await s3
+    .getObject({Bucket: resizedBucket, Key: key})
+    .promise();
 
   return {
-      statusCode: 200,
-      headers: { "Content-Type": "application/" + fileExtension, "Content-Disposition": `attachment; filename=${key}` },
-      body: uploaded.Body?.toString("base64") || "",
-      isBase64Encoded: true,
+    statusCode: 200,
+    headers: {
+      "Content-Type": "application/" + fileExtension,
+      "Content-Disposition": `attachment; filename=${key}`,
+    },
+    body: uploaded.Body?.toString("base64") || "",
+    isBase64Encoded: true,
   };
-};
+}
 
-async function handleResize(fileName, key, dimensions, coldBucket, resizedBucket, s3) {
+async function handleResize(
+  fileName,
+  key,
+  dimensions,
+  coldBucket,
+  resizedBucket,
+  s3,
+) {
   const fileExtension = getExtension(fileName);
 
-  const uploaded = await s3.getObject({ Bucket: coldBucket, Key: fileName }).promise();
+  const uploaded = await s3
+    .getObject({Bucket: coldBucket, Key: fileName})
+    .promise();
 
   const image = await sharp(uploaded.Body)
-      .resize(dimensions.width, dimensions.height)
-      .toBuffer();
+    .resize(dimensions.width, dimensions.height)
+    .toBuffer();
 
-  await s3.upload({ Body: image, Bucket: resizedBucket, Key: key }).promise();
+  await s3.upload({Body: image, Bucket: resizedBucket, Key: key}).promise();
 
   return {
-      statusCode: 200,
-      headers: { "Content-Type": "application/" + fileExtension, "Content-Disposition": `attachment; filename=${key}` },
-      body: image.toString("base64"),
-      isBase64Encoded: true,
+    statusCode: 200,
+    headers: {
+      "Content-Type": "application/" + fileExtension,
+      "Content-Disposition": `attachment; filename=${key}`,
+    },
+    body: image.toString("base64"),
+    isBase64Encoded: true,
   };
-};
+}
 
-
-exports.handler = async (event) => {
+exports.handler = async event => {
   console.log("event handler ", event);
-    const fileName = event.pathParameters?.file;
-    const size = event.queryStringParameters?.size;
+  const fileName = event.pathParameters?.file;
+  const size = event.queryStringParameters?.size;
 
-    if (!fileName) throw Error("No file name provided");
-    if (!size) return await handleNoSize(fileName, RESIZED_BUCKET, s3);
+  if (!fileName) throw Error("No file name provided");
+  if (!size) return await handleNoSize(fileName, RESIZED_BUCKET, s3);
 
-    if (ALLOWED_DIMENSIONS.size > 0 && !ALLOWED_DIMENSIONS.has(size)) return { statusCode: 403, headers: {}, body: "" };
+  if (ALLOWED_DIMENSIONS.size > 0 && !ALLOWED_DIMENSIONS.has(size))
+    return {statusCode: 403, headers: {}, body: ""};
 
-    const resizedKey = size + "." + fileName;
+  const resizedKey = size + "." + fileName;
 
-    try {
-        return await handleResized(resizedKey, RESIZED_BUCKET, s3);
-    } catch {
-        const split = size.split("x");
+  try {
+    return await handleResized(resizedKey, RESIZED_BUCKET, s3);
+  } catch {
+    const split = size.split("x");
 
-        return await handleResize(fileName, resizedKey, { width: parseInt(split[0]), height: parseInt(split[1]) }, RESIZED_BUCKET, RESIZED_BUCKET, s3);
-    }
+    return await handleResize(
+      fileName,
+      resizedKey,
+      {width: parseInt(split[0]), height: parseInt(split[1])},
+      RESIZED_BUCKET,
+      RESIZED_BUCKET,
+      s3,
+    );
+  }
 };
